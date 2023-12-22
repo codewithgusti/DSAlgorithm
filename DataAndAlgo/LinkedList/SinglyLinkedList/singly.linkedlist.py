504c1bf066909f0ebf1867992a41af90e5e5848e
codewithsyre <augustineshoks7@gmail.com>
2023-12-22 21:18:25 +0200

    Initial commit

--- DataAndAlgo/LinkedList/SinglyLinkedList/singly.linkedlist.py
+++ DataAndAlgo/LinkedList/SinglyLinkedList/singly.linkedlist.py
@@ -18,11 +18,35 @@ class SinglyLinkedList:
         else:
             self.tail = node
             self.head = node
+            
+    def append_at_a_location(self,data, index):
+        current = self.head
+        prev = self.head
+        node = Node(data)
+        count = 1
+        while current:
+            if count == 1:
+                node.next = current
+                self.head = node
+                return
+            
+            elif index == index:
+                node.next = current 
+                prev.next = node
+                return
+            count +=1
+            prev=count
+            count=count.next
+        if count < index:
+            print('The list has less number of elements')
+        
+        
 
 words = SinglyLinkedList()
 words.append('apple')
 words.append('banana')
 words.append('orange')
+words.append_at_a_location('new',2)
 
 current = words.head
 while current:
